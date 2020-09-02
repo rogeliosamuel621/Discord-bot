f08f6cec296c1a7e5dadfb4021c4bf011e88ae21
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-09-02 14:05:35 -0600

    [ADD] Input component.

---
+++ src/container/Intput.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <main>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <button>Change password</button>
+        <button>Start</button>
+      </div>
+    </main>
+  );
+};
+
+export default Input;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Output from "../container/Output";
+import Input from "../container/Intput";
 
 const Home = () => {
   return (
     <>
       <Output />
+      <Input />
     </>
   );
 };
