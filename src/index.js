30ba1bdfbb2b7fc1dfbb23ab9e170e065954982d
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2020-09-05 16:59:29 -0600

    [ADD] react function, and message embed.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-const { Client } = require("discord.js");
+const { Client, MessageEmbed } = require('discord.js');
 const client = new Client();
-const { discorToken } = require("./config");
+const { discorToken } = require('./config');
 
-client.on("ready", () => {
+client.on('ready', () => {
   console.log(`logged in as ${client.user.tag}`);
 });
 
-client.on("message", (msg) => {
-  if (msg.content === "Hola") {
-    msg.reply("Hola!!");
+// client.on('message', (msg) => {
+//   if (msg.content === 'Hola') {
+//     msg.reply('Hola!!');
+//   }
+// });
+
+client.on('message', (msg) => {
+  if (msg.content.toLocaleLowerCase() === 'hola') {
+    msg.react('✌');
+    const embed = new MessageEmbed()
+      .setTitle('Welcome')
+      .setColor('133b5c')
+      .setDescription(`¡¡Enjoy the server!! ${msg.author}`);
+    msg.channel.send(embed);
   }
 });
 
